37c177c5ef8fdf0c0ac77dbf110a512456506076
stalin velchael mendez <115166348+Velchael@users.noreply.github.com>
2023-09-04 10:55:46 -0300

    Criação da pasta middlewarw, archivo: tasksMiddleware.js para validar 'title, status'

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router();
 const tasksController = require('./controllers/tasksController');
+const tasksMiddleware = require('./middlewares/tasksMiddleware');
 
 router.get('/tasks', tasksController.getAll);
-router.post('/tasks', tasksController.createTask);
+router.post('/tasks', tasksMiddleware.validateFieldTitle,tasksController.createTask);
 module.exports = router;
