b8ab6c1b5ac9ff28aea161bd1909057e97518eab
stalin velchael mendez <115166348+Velchael@users.noreply.github.com>
2023-09-20 11:48:57 -0300

    Adicionar API do ClimaTempo no frontend:Criação de pasta animated

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -68,6 +68,7 @@ const registerUser = async (event) => {
   registrationMessage.textContent = '';
   registrationMessagedos.textContent = '';
   inputLoginName.value = '';
+  usertarefa.innerHTML = '';
   event.preventDefault();
   // Crear el objeto user con los valores de los inputs
   const user = { username: inputName.value, email: inputEmail.value, password: inputPassword.value };
@@ -102,11 +103,9 @@ const registerUser = async (event) => {
     registrationMessagedos.style.color = 'red';
     registrationMessagedos.style.display = 'block';
   }
-//registrationMessagedos.textContent = '';
 inputName.value = '';
 inputEmail.value = '';
 inputPassword.value = '';
-
 loadTasks();
 }
 // Vincular la función al evento submit del formulario
@@ -119,17 +118,13 @@ const fetchTasksLogin = async (username) => {
     if (!response.ok) {
       throw new Error('Error al obtener tareas.');
     }
-
     const tasks = await response.json();
-    
     return tasks;
   } catch (error) {
     console.error("Error en fetchTasksLogin:", error.message);
     return []; // Retorna un array vacío en caso de error.
   }
-  
 };
-
 
 const addTask = async (event) => {
   event.preventDefault();
@@ -259,9 +254,65 @@ const tasks = await fetchTasksLogin(user.username);
     const tr = createRow(task);
     tbody.appendChild(tr);
   });
-  
 }
-
 addForm.addEventListener('submit', addTask);
 
 loadTasks();
+
+window.addEventListener('load', () => {
+  let lon;
+  let lat;
+  let temperaturaValor = document.getElementById('temperatura-valor');
+  let temperaturaDescripcion = document.getElementById('temperatura-descripcion');
+  let ubicacion = document.getElementById('ubicacion');
+  let iconoAnimado = document.getElementById('icono-animado');
+  let vientoVelocidad = document.getElementById('viento-velocidad');
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(posicion => {
+          lon = posicion.coords.longitude;
+          lat = posicion.coords.latitude;
+          // Consultar la API utilizando la ubicación actual
+          const apiKey = '8b12dff123a0cfc07e1d6a853eb3c5bd'; // Reemplaza con tu clave de API real
+          const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=${apiKey}`;
+          fetch(url)
+              .then(response => response.json())
+              .then(data => {
+                  let temp = Math.round(data.main.temp);
+                  temperaturaValor.textContent = `${temp} ° C`;
+                  let desc = data.weather[0].description;
+                  temperaturaDescripcion.textContent = desc.toUpperCase();
+                  ubicacion.textContent = data.name;
+                  vientoVelocidad.textContent = `${data.wind.speed} m/s`;
+                  // Para iconos dinámicos
+                  switch (data.weather[0].main) {
+                      case 'Thunderstorm':
+                          iconoAnimado.src = 'animated/thunder.svg';
+                          break;
+                      case 'Drizzle':
+                          iconoAnimado.src = 'animated/rainy-2.svg';
+                          break;
+                      case 'Rain':
+                          iconoAnimado.src = 'animated/rainy-7.svg';
+                          break;
+                      case 'Snow':
+                          iconoAnimado.src = 'animated/snowy-6.svg';
+                          break;
+                      case 'Clear':
+                          iconoAnimado.src = 'animated/day.svg';
+                          break;
+                      case 'Atmosphere':
+                          iconoAnimado.src = 'animated/weather.svg';
+                          break;
+                      case 'Clouds':
+                          iconoAnimado.src = 'animated/cloudy-day-1.svg';
+                          break;
+                      default:
+                          iconoAnimado.src = 'animated/cloudy-day-1.svg';
+                  }
+              })
+              .catch(error => {
+                  console.log(error);
+              });
+      });
+  }
+});
